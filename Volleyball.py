fa32c6360e74fdb6a45792d1a09320e067dd6d96
AsaM22 <77682622+AsaM22@users.noreply.github.com>
2021-06-17 11:57:38 -0400

    Inital commit

---
+++ Volleyball.py
@@ -0,0 +1,66 @@
+
+# TODO: Make the printing look nice
+
+class Volleyball:
+    def __init__(self, input_players):
+        self._player_list = input_players
+        self._starting_player_list = input_players
+
+    def rotate(self) -> None:
+        """Rotates the players in the list like a volleyball game rotation"""
+        self._player_list.insert(0, self._player_list.pop(len(self._player_list)-1))
+        
+
+
+    def get_subs(self) -> list:
+        """Returns the current subsitutes in the rotation"""
+        return list(self._player_list[3], self._player_list[7])
+
+
+    def get_active_line_only(self) -> list:
+        """Returns the current active players on the field"""
+        temp_list = self._player_list.copy()
+
+        # Removes the sub list
+        temp_list.pop(3)
+        temp_list.pop(6)
+
+        return temp_list
+
+    
+    def get_full_cycle_rotation(self) -> list:
+        """Returns a list of lists for each rotation available"""
+        temp_list = []
+
+        for _ in range(len(self._starting_player_list)):
+            temp_list.append(self._player_list)
+
+            self.rotate()
+
+
+
+        # To fix the rotation back to normal
+        self.rotate()
+
+        return temp_list
+
+
+    def get_starting_line(self) -> list:
+        return self._starting_player_list
+
+    def get_current_rotation(self) -> list:
+        return self._player_list
+
+
+if __name__ == '__main__':
+    game = Volleyball(['Gabby', 'Connor', 'Maggie', 'Clark', 'Cooper', 'Rossi', 'Asa', 'Cece'])
+
+
+    x = game.get_full_cycle_rotation()
+
+    for i in x:
+        print(i)
+
+
+
+
