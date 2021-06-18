d03ccdd36a33c9566043e81ef62719ee406e5082
AsaM22 <77682622+AsaM22@users.noreply.github.com>
2021-06-17 21:52:36 -0400

    Adds check for not having enough after remove

--- Volleyball.py
+++ Volleyball.py
@@ -5,8 +5,17 @@
 
 class Volleyball:
     def __init__(self, input_players: list):
-        self._player_list = input_players
+        
+        
+
+        self._player_list = self._turn_title(input_players)
         self._starting_player_list = input_players
+
+
+    def _turn_title(self, input_list: list):
+        return [each_string.title() for each_string in input_list]
+
+        
 
     def rotate(self) -> None:
         """Rotates the players in the list like a volleyball game rotation"""
@@ -20,12 +29,13 @@ class Volleyball:
 
     def get_active_line_only(self) -> list:
         """Returns the current active players on the field"""
-        temp_list = self._player_list.copy()
+        temp_list = self._player_list[:]
 
+        # TODO: need to check for how many players there are first
         # Removes the sub list
-        temp_list.pop(3)
         temp_list.pop(6)
-
+        temp_list.pop(len(temp_list)-3)
+        
         return temp_list
 
     
@@ -44,23 +54,34 @@ class Volleyball:
 
 
     def get_starting_line(self) -> list:
+        """Returns starting 6 players position"""
         return self._starting_player_list
 
     def get_current_rotation(self) -> list:
+        """Returns the current curation of players"""
         return self._player_list
 
     def remove_player(self, player_name: str) -> None:
         """Removes player from the current rotation"""
         
         try:
-            if player_name in self._player_list:
-                pass
-            self._player_list.remove(player_name)
+            if player_name.title() in self._player_list:
+                self._player_list.remove(player_name.title())
 
+            if len(self.get_active_line_only()) < 6:
+                print('Now you don\'t have enough players')
+                
         except ValueError:
             print('Sorry that player doesn\'t exist')
 
 if __name__ == '__main__':
-    game = Volleyball(['Gabby', 'Connor', 'Maggie', 'Clark', 'Cooper', 'Rossi', 'Asa', 'Cece'])
+    game = Volleyball(['Gabby', 'Connor', 'maGgie', 'Clark', 'Cooper', 'Rossi', 'Asa', 'Cece'])
+
+    print(game.get_active_line_only())
+
+    game.remove_player('MagGiE')
+
+    print(game.get_active_line_only())
+
 
 
