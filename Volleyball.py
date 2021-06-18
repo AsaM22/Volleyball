15611407bd7d407e6edcda1d0c5f1db751974e06
AsaM22 <77682622+AsaM22@users.noreply.github.com>
2021-06-17 21:38:35 -0400

    Adds 'remove_player' function

--- Volleyball.py
+++ Volleyball.py
@@ -1,8 +1,10 @@
 
+
+
 # TODO: Make the printing look nice
 
 class Volleyball:
-    def __init__(self, input_players):
+    def __init__(self, input_players: list):
         self._player_list = input_players
         self._starting_player_list = input_players
 
@@ -10,7 +12,6 @@ class Volleyball:
         """Rotates the players in the list like a volleyball game rotation"""
         self._player_list.insert(0, self._player_list.pop(len(self._player_list)-1))
         
-
 
     def get_subs(self) -> list:
         """Returns the current subsitutes in the rotation"""
@@ -33,11 +34,8 @@ class Volleyball:
         temp_list = []
 
         for _ in range(len(self._starting_player_list)):
-            temp_list.append(self._player_list)
-
+            temp_list.append(str(self._player_list))
             self.rotate()
-
-
 
         # To fix the rotation back to normal
         self.rotate()
@@ -51,16 +49,18 @@ class Volleyball:
     def get_current_rotation(self) -> list:
         return self._player_list
 
+    def remove_player(self, player_name: str) -> None:
+        """Removes player from the current rotation"""
+        
+        try:
+            if player_name in self._player_list:
+                pass
+            self._player_list.remove(player_name)
+
+        except ValueError:
+            print('Sorry that player doesn\'t exist')
 
 if __name__ == '__main__':
     game = Volleyball(['Gabby', 'Connor', 'Maggie', 'Clark', 'Cooper', 'Rossi', 'Asa', 'Cece'])
-
-
-    x = game.get_full_cycle_rotation()
-
-    for i in x:
-        print(i)
-
-
 
 
